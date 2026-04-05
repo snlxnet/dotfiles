8f6b125a50dc7fe4b82583da556738ea415e079d
Alex <alexanderthestudent@yandex.ru>
2026-04-05 15:44:34 +0300

    base ui settings

--- flake.nix
+++ flake.nix
@@ -10,8 +10,6 @@
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
     configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
       environment.systemPackages =
         [ pkgs.vim
         ];
@@ -20,16 +18,28 @@
       nix.settings.experimental-features = "nix-command flakes";
 
       # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
+      programs.fish.enable = true;
 
-      # Set Git commit hash for darwin-version.
+      system.primaryUser = "alex";
+
+      system.defaults.dock = {
+        autohide = true;
+        orientation = "left";
+        show-recents = false;
+        persistent-apps = [
+          { app = "/Applications/Safari.app"; }
+        ];
+      };
+      system.defaults.NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        "com.apple.swipescrolldirection" = false;
+      };
+
+      # The do-not-touch section
       system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
       system.stateVersion = 6;
-
-      # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
     };
   in
