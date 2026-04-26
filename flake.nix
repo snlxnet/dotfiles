9569ab2cfd5b34ed0885ad7fb3471c4c1d5e477a
Alex <alexanderthestudent@yandex.ru>
2026-04-26 19:05:48 +0300

    update & install ollama

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
+    unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
@@ -13,7 +14,7 @@
     };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, unstable, home-manager }:
   {
     darwinConfigurations."saturn" = nix-darwin.lib.darwinSystem {
       modules = [
@@ -23,6 +24,11 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.alex = import ./home.nix;
+        }
+        {
+          environment.systemPackages = [
+            unstable.legacyPackages.aarch64-darwin.ollama
+          ];
         }
       ];
     };
