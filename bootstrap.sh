f4126b0a1a3149a66c26f5d1e0d1fc89eb4f7c4f
Alex <alexanderthestudent@yandex.ru>
2026-05-02 14:04:39 +0300

    fix: bootstrap scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,4 +8,7 @@ ___] |  |  |  |__| |  \ | \|    |__] |__| |__|  |  ___]  |  |  \ |  | |
 echo "Installing NIX"
 sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install)
 
-echo "run zsh setup-nix-darwin.sh"
+echo "Installing Homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "run zsh setup-nix-darwin.sh after a reboot"
